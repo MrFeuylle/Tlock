6e215f84d1f403fdba215c6f74570823f21ae098
xatre0 <113340829+xatre0@users.noreply.github.com>
2023-08-01 14:16:25 +0200

    Add files via upload

--- TLock.sh
+++ TLock.sh
@@ -1,10 +1,10 @@
-echo "echo Tu a oublier de lock" >> ~/.zshrc
+echo "echo Tu as oublie de lock" >> ~/.zshrc
 clear
-zenity --title "Tu a oublier de lock ta session" --height=200 --width=800 --warning --text="<span color=\"red\"><span size=\"80000\">Oups, un API a vu que tu n'as pas lock</span></span>"
+zenity --title "Tu as oublie de lock ta session" --height=200 --width=800 --warning --text="<span color=\"red\"><span size=\"80000\">Oups, un API a vu que tu n'as pas lock</span></span>"
 zenity --title "Question" --height=200 --width=800 --question --text="<span size=\"20000\">Sais-tu pourquoi penser a lock ta session quand tu n'es pas devant est important ?\nDo you know why locking your session when you're not here is important ?</span>"
 if [ $? -eq 1 ]
 then
-	zenity --title "Explication" --height=100 --width=300 --info --text="<span size=\"20000\">Cela veut dire que n'importe qui peut acceder a tout tes dossiser / mot de passe / compte qui sont sur ton ordinateur et c'est pas cool.\nNe pas lock sa session, c'est risquez de tout perdre ou de se faire voler ses données\nThis means that anyone can access all your folders / passwords / accounts that are on your computer and that's not cool.\nNot locking your session means to risk losing everything and having all your data stolen.</span>"
+	zenity --title "Explication" --height=100 --width=300 --info --text="<span size=\"20000\">Cela veut dire que n'importe qui peut acceder a tout tes dossiers / mots de passe / comptes qui sont sur ton ordinateur et c'est pas cool.\nNe pas lock sa session, c'est risquer de tout perdre ou de se faire voler ses données\nThis means that anyone can access all your folders / passwords / accounts that are on your computer and that's not cool.\nNot locking your session means you risk losing everything and having all your data stolen.</span>"
 fi
 zenity --title "API love you" --height=100 --width=300 --info --text="<span size=\"20000\">Voila comment il faut faire\nHere's how to do it</span>"
 ft_lock
