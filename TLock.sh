637542f65d9b352265471361df91449fd901ee89
xatre0 <113340829+xatre0@users.noreply.github.com>
2023-08-01 15:43:21 +0200

    Update TLock.sh

--- TLock.sh
+++ TLock.sh
@@ -4,7 +4,7 @@ zenity --title "Tu as oublie de lock ta session" --height=200 --width=800 --warn
 zenity --title "Question" --height=200 --width=800 --question --text="<span size=\"20000\">Sais-tu pourquoi penser a lock ta session quand tu n'es pas devant est important ?\nDo you know why locking your session when you're not here is important ?</span>"
 if [ $? -eq 1 ]
 then
-	zenity --title "Explication" --height=100 --width=300 --info --text="<span size=\"20000\">Cela veut dire que n'importe qui peut acceder a tout tes dossiers / mots de passe / comptes qui sont sur ton ordinateur et c'est pas cool.\nNe pas lock sa session, c'est risquer de tout perdre ou de se faire voler ses données\nThis means that anyone can access all your folders / passwords / accounts that are on your computer and that's not cool.\nNot locking your session means you risk losing everything and having all your data stolen.</span>"
+	zenity --title "Explication" --height=100 --width=300 --info --text="<span size=\"20000\">Cela veut dire que n'importe qui peut acceder a touts tes dossiers / mots de passe / comptes qui sont sur ton ordinateur et c'est pas cool.\nNe pas lock sa session, c'est risquer de tout perdre ou de se faire voler ses données\nThis means that anyone can access all your folders / passwords / accounts that are on your computer and that's not cool.\nNot locking your session means you risk losing everything and having all your data stolen.</span>"
 fi
 zenity --title "API love you" --height=100 --width=300 --info --text="<span size=\"20000\">Voila comment il faut faire\nHere's how to do it</span>"
 ft_lock
